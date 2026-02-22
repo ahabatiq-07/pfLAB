123922e8689b6c0d470a23d5aad4f7c82da82121
ahabatiq-07 <ahabatiq@gmail.com>
2026-02-23 03:09:49 +0500

    lab1

---
+++ text19.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main(){
+system("color 09");
+cout<<"             +--^--------,---------,------,-------------^-,                                             "<<endl;
+cout<<"             |  ||||||||  '---------'       |              O                                            "<<endl;
+cout<<"             ,+-----------------------------^--------------|                                            "<<endl;
+cout<<"               '-----------,-----------,----------------'                                               "<<endl;
+cout<<"               / XXXXXX   /'|      /'                                                                   "<<endl;
+cout<<"              /  XXXXX   /  |     / '                                                                   "<<endl;
+cout<<"             /  XXXXX   /'------'                                                                       "<<endl;
+cout<<"            /   XXXX   /                                                                                "<<endl;
+cout<<"           /    XX    /                                                                                 "<<endl;
+cout<<"          (----------(                                                                                  "<<endl;
+cout<<"            '-----'                                                                                     "<<endl;
+}
